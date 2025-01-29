23dd51cf744a88e7cf668ed425a261890d3c1717
theia <theia@mit.edu>
2025-01-28 21:45:34 -0500

    fixed export names

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,7 +1,7 @@
-import type { GraffitiSolidOIDCInterfaceOptions } from "../types";
+import type { GraffitiSolidOIDCSessionManagerOptions } from "../types";
 import { GraffitiLocalSessionManager } from "@graffiti-garden/implementation-local/session-manager";
 // TODO: actually use solid for node version
 // import { getSessionFromStorage } from "@inrupt/solid-client-authn-node";
 
-export type { GraffitiSolidOIDCInterfaceOptions };
-export { GraffitiLocalSessionManager as GraffitiSolidOIDCInterface };
+export type { GraffitiSolidOIDCSessionManagerOptions };
+export { GraffitiLocalSessionManager as GraffitiSolidOIDCSessionManager };
