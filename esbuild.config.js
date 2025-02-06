36939711fef1f371adcdae860f369b0ee1024b46
theia <theia@mit.edu>
2025-02-05 21:59:57 -0500

    full grid of esm/cjs/node/browser

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,33 +1,29 @@
 import { rmSync } from "fs";
 import * as esbuild from "esbuild";
 
-// Clean dist folder
-rmSync("dist", { recursive: true, force: true });
+for (const format of ["esm", "cjs"]) {
+  await esbuild.build({
+    entryPoints: ["src/browser/index.ts"],
+    platform: "browser",
+    bundle: true,
+    sourcemap: true,
+    minify: true,
+    splitting: format === "esm",
+    format,
+    outdir: `dist/browser/${format}`,
+    loader: {
+      ".html": "text",
+      ".css": "text",
+      ".webp": "dataurl",
+      ".woff2": "dataurl",
+    },
+  });
 
-await esbuild.build({
-  entryPoints: ["src/browser/index.ts"],
-  platform: "browser",
-  bundle: true,
-  sourcemap: true,
-  minify: true,
-  splitting: true,
-  format: "esm",
-  target: "esnext",
-  outdir: "dist/browser",
-  loader: {
-    ".html": "text",
-    ".css": "text",
-    ".webp": "dataurl",
-    ".woff2": "dataurl",
-  },
-});
-
-await esbuild.build({
-  entryPoints: ["src/node/index.ts"],
-  platform: "node",
-  sourcemap: true,
-  minify: true,
-  format: "cjs",
-  target: "esnext",
-  outdir: "dist/node",
-});
+  await esbuild.build({
+    entryPoints: ["src/node/index.ts"],
+    platform: "node",
+    sourcemap: true,
+    minify: true,
+    outdir: `dist/node/${format}`,
+  });
+}
